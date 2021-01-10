43072281a42967f47076542682716298f34d06e9
NanoCarbon <edward.w.lin@gmail.com>
2021-01-10 15:30:26 -0500

    Before Linked Lists

---
+++ Array.java
@@ -0,0 +1,56 @@
+
+public class Array{
+
+    private int[] items;
+    private int count;
+
+
+    public Array(int length){
+        items = new int[length];    
+    }
+
+    public void print(){
+
+        for (int i = 0; i<count; i++){
+            System.out.println(items[i]);
+        }
+    }
+
+    public void removeAt(int index){
+        //Validate the index
+        if(index<0 || index>=count){
+            throw new IllegalArgumentException();
+        }
+
+        //Shift the items to the left to fill the hole
+        for (int i=index;i<count;i++){
+            items[i]=items[i+1];
+        }
+        count--;
+    }
+
+    public int indexOf(int item){
+        for (int i =0; i<count;i++){
+            if (items[i]==item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void insert(int item){
+        // If the array is full, resize it
+        if(items.length == count){
+
+            int[] newItems = new int[count * 2];
+            for (int i=0; i<count; i++){
+                newItems[i] = items[i];
+            }
+            items = newItems;
+
+        }
+
+        items[count++] = item;
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,11 +1,31 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
-        int[] numbers = new int[3];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        System.out.println(Arrays.toString(numbers));
+        //Creating an Array Class - Coding with Mosh
+        /*
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        System.out.println(numbers.indexOf(100));
+        numbers.print();
+        */
+
+        //ArrayLists
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(20);
+        /* Various ArrayList Methods
+        list.remove(0);
+        list.indexOf(20);
+        list.lastIndexOf(20);
+        list.size();
+        list.toArray();
+        */
+        System.out.println(list);
     }
 }
